e7f8ca6dd7b3e3aceb57deb7c2a87152f923a948
Stephania Pucci <94070946+StephaniaPucci@users.noreply.github.com>
2025-04-30 20:11:27 -0300

    Delete bootstrap directory

--- bootstrap/jquery.js
+++
@@ -1,7 +0,0 @@
-let botao = document.querySelector("#botao");
-let frase = document.querySelector("#frase");
-
-document.getElementById("botao").addEventListener('click',
-    function() {
-        document.getElementById('frase').style.display = 'block';
-    });
