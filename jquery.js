d27ff3a4b4fdc4f241c238086667e7fff248b455
Stephania Pucci <94070946+StephaniaPucci@users.noreply.github.com>
2025-05-06 14:38:40 -0300

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -4,4 +4,10 @@ let frase = document.querySelector("#frase");
 document.getElementById("botao").addEventListener('click',
     function() {
         document.getElementById('frase').style.display = 'block';
+        const apagarfrase = 5000;
+        const frase = document.getElementById("frase");
+        setTimeout(function() {
+            frase.textContent = '';
+        }, apagarfrase);
     });
+   
